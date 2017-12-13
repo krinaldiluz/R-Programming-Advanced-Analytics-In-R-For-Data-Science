ecd848b2ad17c705d948b73a675ae33d0897a9f2
Kelvin Rinaldi <34136901+krinaldiluz@users.noreply.github.com>
2017-12-13 14:02:17 -0200

    Update Lists in R.R

--- Lists in R.R
+++ Lists in R.R
@@ -4,7 +4,7 @@
 #Character: machine name
 #Vector: (min, mean, max) Utilization for the month (excluding unknown hours)
 #Logical: Has utilization ever fallen below 90%? TRUE/FALSE
-#Vector: All hoours where utilization is unknown (NA's)
+#Vector: All hours where utilization is unknown (NA's)
 #Dataframe: For this machine
 #Plot: For all machines
 
@@ -33,3 +33,46 @@ util$PosixTime <- as.POSIXct(util$Timestamp, format="%d/%m/%Y %H:%M")
 util$Timestamp <- NULL
 # Trocando a ordem das colunas
 util <- util[,c(4,1,2,3)]
+
+#What is a list?
+summary(util)
+
+RL1 <- util[util$Machine=="RL1",]
+
+summary(RL1)
+
+RL1$Machine <- factor(RL1$Machine)
+
+#Character: machine name
+#Vector: (min, mean, max) Utilization for the month (excluding unknown hours)
+#Logical: Has utilization ever fallen below 90%? TRUE/FALSE
+
+util_stats_rl1 <- c(min(RL1$Utilization, na.rm=T), 
+                    mean(RL1$Utilization, na.rm=T),
+                    max(RL1$Utilization, na.rm=T))
+
+util_stats_rl1
+
+util_under_90_flag <- length(which(RL1$Utilization < 0.90)) > 0
+
+util_under_90_flag
+
+list_rl1 <- list("RL1", util_stats_rl1, util_under_90_flag)
+
+list_rl1
+
+#Naming components of a list
+
+names(list_rl1)
+
+names(list_rl1) <- c("Machine", "Stats", "LowThresold")
+
+##Another way. Like with dataframes:
+
+## Removing a variable
+rm(list_rl1)
+
+##Assigning names during the list creation
+list_rl1 <- list(Machine="RL1", Stats=util_stats_rl1, LowThresold=util_under_90_flag)
+
+list_rl1
