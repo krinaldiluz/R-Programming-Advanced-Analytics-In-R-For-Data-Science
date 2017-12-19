668e72da05ba2715e61bb5cdef4188170f075a7d
Kelvin Rinaldi <kelvinr.rinaldi@neogrid.com>
2017-12-19 13:36:52 -0200

    RCommit

---
+++ Apply Functions.R
@@ -0,0 +1,36 @@
+getwd()
+setwd("./Weather-Data/Weather Data")
+
+#Read Data
+#row.names = We are giving names for the rows in the matrices
+Chicago <- read.csv("Chicago-F.csv", row.names=1)
+NewYork <- read.csv("NewYork-F.csv", row.names=1)
+Houston <- read.csv("Houston-F.csv", row.names=1)
+SanFrancisco <- read.csv("SanFrancisco-F.csv", row.names=1)
+
+Chicago
+NewYork
+Houston
+SanFrancisco
+
+#these are dataframes:
+is.data.frame(Chicago)
+
+#Let's convert to matrices:
+Chicago <- as.matrix(Chicago)
+NewYork <- as.matrix(NewYork)
+Houston <- as.matrix(Houston)
+SanFrancisco <- as.matrix(SanFrancisco)
+
+#check
+is.matrix(Chicago)
+
+#Creating the list
+Weather <- list(Chicago=Chicago, NewYork=NewYork, Houston=Houston, SanFrancisco=SanFrancisco)
+
+Weather
+
+#Let's try it out:
+Weather[3]
+Weather[[3]]
+Weather$Houston
